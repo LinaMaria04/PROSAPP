875cadc5c769394564bb92cf6e599971ea184b3d
Prosarc <sistemas@prosarc.com.co>
2025-04-03 15:54:44 -0500

    Creacion de proyecto

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});*/
+Route::get('/', function () {
+    return view('home');
 });
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
